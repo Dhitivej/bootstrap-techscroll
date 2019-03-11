f60b554d61f8528f140bc827b2c95551fe3bb03b
Dhitivej Chuersuwan <d_chuersuwan@.com>
2019-03-11 09:19:42 -0400

    Update for jQuery v3.3.1

--- main.js
+++ main.js
@@ -47,15 +47,13 @@ sr.reveal('.info-left', {
 
 // Smooth Scrolling
 
-$('a[href*="#"]:not([href="#"])').click(function () {
-     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-               $('html, body').animate({
-                    scrollTop: target.offset().top
-               }, 1000);
-               return false;
-          }
+$('.navbar a').on('click', function (e) {
+     if (this.hash !== "") {
+          e.preventDefault();
+
+          const hash = this.hash;
+          $('html, body').animate({
+               scrollTop: $(hash).offset().top
+          }, 800)
      }
-});
+})
